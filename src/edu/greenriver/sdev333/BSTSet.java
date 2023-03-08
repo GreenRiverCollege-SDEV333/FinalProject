40ab0c340594cb0b47026691859031bea08a2da7
adamwise <awise.developer@gmail.com>
2023-03-08 14:16:29 -0800

    added BSTSet class

--- src/edu/greenriver/sdev333/BSTSet.java
+++ src/edu/greenriver/sdev333/BSTSet.java
@@ -1,73 +1,113 @@
 package edu.greenriver.sdev333;
 
-/**
- * @author Adam Wise
- *
- * A MathSet represents a finite mathematical set.
- * Sets have a collection of unique elements (keys) - no duplicate keys allowed.
- * Set operations include contains, size, union, intersection, and difference.
- * Set complement and element (key) deletion is not supported by this API.
- * @param <KeyType>
- */
-public interface MathSet<KeyType> {
+public class BSTSet<KeyType> implements MathSet<KeyType>{
+
+    private Node root;
+    //helper class
+    private class Node {
+        private KeyType key;
+
+        private Node left;
+        private Node right;
+        private int N; // Number of Nodes in the subtree rooted here
+
+        public Node(KeyType key,  int N){
+            this.key = key;
+            this.N = N;
+        }
+
+    }
+
     /**
      * Puts the specified key into the set.
+     *
      * @param key key to be added into the set
      */
-    void add(KeyType key);
+    @Override
+    public void add(KeyType key) {
+
+    }
 
     /**
      * Is the key in the set?
+     *
      * @param key key to check
      * @return true if key is in the set, false otherwise
      */
-    boolean contains(KeyType key);
+    @Override
+    public boolean contains(KeyType key) {
+        return false;
+    }
 
     /**
      * Is the set empty?
+     *
      * @return true if the set is empty, false otherwise
      */
-    boolean isEmpty();
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 
     /**
      * Number of keys in the set
+     *
      * @return number of keys in the set.
      */
-    int size();
+    @Override
+    public int size() {
+        return 0;
+    }
 
     /**
      * Determine the union of this set with another specified set.
      * Returns A union B, where A is this set, B is other set.
      * A union B = {key | A.contains(key) OR B.contains(key)}.
      * Does not change the contents of this set or the other set.
+     *
      * @param other specified set to union
      * @return the union of this set with other
      */
-    MathSet<KeyType> union(MathSet<KeyType> other);
+    @Override
+    public MathSet<KeyType> union(MathSet<KeyType> other) {
+        return null;
+    }
 
     /**
      * Determine the intersection of this set with another specified set.
      * Returns A intersect B, where A is this set, B is other set.
      * A intersect B = {key | A.contains(key) AND B.contains(key)}.
      * Does not change the contents of this set or the other set.
+     *
      * @param other specified set to intersect
      * @return the intersection of this set with other
      */
-    MathSet<KeyType> intersection(MathSet<KeyType> other);
+    @Override
+    public MathSet<KeyType> intersection(MathSet<KeyType> other) {
+        return null;
+    }
 
     /**
      * Determine the difference of this set with another specified set.
      * Returns A difference B, where A is this set, B is other set.
      * A difference B = {key | A.contains(key) AND !B.contains(key)}.
      * Does not change the contents of this set or the other set.
+     *
      * @param other specified set to difference
      * @return the difference of this set with other
      */
-    MathSet<KeyType> difference(MathSet<KeyType> other);
+    @Override
+    public MathSet<KeyType> difference(MathSet<KeyType> other) {
+        return null;
+    }
 
     /**
      * Retrieves a collection of all the keys in this set.
+     *
      * @return a collection of all keys in this set
      */
-    Iterable<KeyType> keys();
+    @Override
+    public Iterable<KeyType> keys() {
+        return null;
+    }
 }
