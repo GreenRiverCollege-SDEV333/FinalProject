295f97322678eac84fa4a12cb373639f8b1b5c28
Stewart Lovell <lovell.stewart@student.greenriver.edu>
2023-03-08 14:16:34 -0800

    added name to mathset

--- src/edu/greenriver/sdev333/MathSet.java
+++ src/edu/greenriver/sdev333/MathSet.java
@@ -1,6 +1,10 @@
 package edu.greenriver.sdev333;
 
 /**
+ * Stewart Lovell
+ */
+
+/**
  * A MathSet represents a finite mathematical set.
  * Sets have a collection of unique elements (keys) - no duplicate keys allowed.
  * Set operations include contains, size, union, intersection, and difference.
