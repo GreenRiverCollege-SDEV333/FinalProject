57b20a82b661e21c814c305be43e32110f15392a
Jrome <jromeall@users.noreply.github.com>
2023-03-20 20:40:11 -0700

    tried testing seperate chaining hashst

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import edu.greenriver.sdev333.BSTSet;
 import edu.greenriver.sdev333.MathSet;
+import edu.greenriver.sdev333.SeparateChainingHashST;
 
 public class Main {
     public static void main(String[] args) {
@@ -35,7 +36,44 @@ public class Main {
         assert(result3.contains("cat"));
         assert(!result3.contains("dog"));
         assert(!result3.contains("mouse"));
+
+        //test number 2
+
+        SeparateChainingHashST<String> st1 = new SeparateChainingHashST<>();
+        SeparateChainingHashST<String> st2 = new SeparateChainingHashST<>();
+
+        st1.put("a", 1);
+        st1.put("b", 2);
+        st1.put("c", 3);
+
+        st2.put("c", 4);
+        st2.put("d", 5);
+        st2.put("e", 6);
+
+        // Testing union
+        SeparateChainingHashST<String> union = st1.union(st2);
+        System.out.println("Union:");
+        for (String key : union.keys()) {
+            System.out.println(key + ": " + union.get(key));
+        }
+
+        // Testing intersection
+        SeparateChainingHashST<String> intersection = st1.intersection(st2);
+        System.out.println("Intersection:");
+        for (String key : intersection.keys()) {
+            System.out.println(key + ": " + intersection.get(key));
+        }
+
+        // Testing difference
+        SeparateChainingHashST<String> difference = st1.difference(st2);
+        System.out.println("Difference:");
+        for (String key : difference.keys()) {
+            System.out.println(key + ": " + difference.get(key));
+        }
+
     }
+
+
 
 
 }
