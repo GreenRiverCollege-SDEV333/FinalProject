266c46a7e83a0892e46c758f7d274f06bedaeeb0
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-03-20 19:20:11 -0700

    change #5

--- src/Main.java
+++ src/Main.java
@@ -52,27 +52,28 @@ public class Main {
         set11.add("C");
 
         MathSet<String> set22 = new BSTSet<>();
+        set22.add("C");
         set22.add("D");
         set22.add("E");
         set22.add("F");
 
-        // union set 1 and set 2, save into result 1
+        // union set 11 and set 22, save into result 11
         MathSet<String> result11 = set11.union(set22);
-        //print out keys from result1
+        //print out keys from result11
         for(String key: result11.keys())
         {
             System.out.println(key);
         }
         System.out.println();
-        result1 = set11.intersection(set22);
-        //print out keys from result1
+        result11 = set11.intersection(set22);
+        //print out keys from result11
         for(String key: result11.keys())
         {
             System.out.println(key);
         }
         System.out.println();
         result11 = set11.difference(set22);
-        //print out keys from result1
+        //print out keys from result11
         for(String key: result11.keys())
         {
             System.out.println(key);
