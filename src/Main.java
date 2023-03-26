4a0ad5b0beac67340f815177496594ba9759b985
junnynoriega <junnynoriega@gmail.com>
2023-03-25 19:39:35 -0700

    Added name to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/**
+ * @Junny Noriega-Arenas
+ * Final Project Part 1
+ * SDEV 333
+ * Description: Create two classes that implements the MathSet interface, which represents a mathematical set.
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
